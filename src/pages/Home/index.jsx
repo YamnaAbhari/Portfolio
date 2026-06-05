63a91846de66ca79bf6462036120598fc590d25b
YamnaAbhari <yamnaabhari@gmail.com>
2026-06-05 18:53:27 +0000

    update home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,6 +6,7 @@ import DesignQualitySection from "./DesignQualitySection";
 import WorkingMethod from "./WorkingMethod";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
+import logo from "../../assets/image/logo.png"
 
 export default function Home() {
   const navigate=useNavigate()
@@ -34,7 +35,6 @@ export default function Home() {
           <h3 className="font-rezvan text-white opacity-80 md:text-2xl text-xl mt-3">
             𝗙𝗿𝗼𝗻𝘁-𝗲𝗻𝗱 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿 / 𝗨𝗶 𝗗𝗲𝘀𝗶𝗴𝗻𝗲𝗿
           </h3>
-
           <div className="sm:hidden mt-3 mb-4 flex gap-3 font-vazir">
               <button onClick={()=>{navigate('/projects')}} className="px-5 py-3 rounded-2xl bg-purple-900 text-white text-[16px]">مشاهده پروژه‌ها</button>
               <button onClick={()=>{navigate('/about')}} className="px-5 py-3 rounded-2xl bg-white/5 text-white text-[16px]  border border-purple-400/30 shadow-[0_0_5px_rgba(168,85,247,0.35)]">درباره من</button>
@@ -53,14 +53,14 @@ export default function Home() {
               target="_blank"
               className="bg-white/80 rounded-lg py-2 px-2  hover:bg-purple-700/80"
             >
-              <FiAtSign className="text-2xl text-purple-700/80 hover:text-white/80 " />
+              <FiAtSign className="text-2xl text-purple-700/80 hover:text-white/80" />
             </a>
           </div>
         </div>
 
         <div className="px-15 py-15 mx-7 lg:mx-0 bg-purple-950/8 backdrop-blur-xl border border-purple-400/30 shadow-[0_0_10px_rgba(168,85,247,0.35)] rounded-3xl">
           <img
-            src="src\assets\image\logo.png"
+            src={logo} alt="logo"
             className="animate-floatUpDown"
           ></img>
         </div>
